83c2667ab77726834626e3d90b2c4b2fd34ee966
Harshit Agrawal <harshitagrawal19@gmail.com>
2022-05-18 16:42:54 +0530

    Add Network regex in convert destination function

--- src/summary/summary.go
+++ src/summary/summary.go
@@ -182,6 +182,10 @@ func networkRegex(str string) (string, error) {
 //convertListofDestination - Create the mapping between Source and Destination/Resource/Protocol
 func convertListofDestination(arr []*sum.ListOfDestination, sysLog types.SystemSummery) []*sum.ListOfDestination {
 	destination := aggregateFolder(sysLog.Resource)
+	//Check Operation is Network
+	if sysLog.Operation == "Network" {
+		destination, _ = networkRegex(sysLog.Resource)
+	}
 	for _, value := range arr {
 		if value.Destination == destination {
 			value.Count += sysLog.Count
